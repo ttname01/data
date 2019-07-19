f2e7dcef6231b7da4f7ddaced6a5b57707a0fe63
chen.yuanming <chen.yuanming@trs.com.cn>
2019-07-20 00:57:15 +0800

    1

---
+++ datamanage/src/main/java/cn/com/datamanage/DatamanageApplication.java
@@ -0,0 +1,13 @@
+package cn.com.datamanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamanageApplication.class, args);
+	}
+
+}
